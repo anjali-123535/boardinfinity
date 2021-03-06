20a16477e204cb7d63b943a64947df046c936036
anjali-123535 <anjaliparihar517@gmail.com>
2021-03-06 16:23:09 +0530

    done with app.js file setup

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log("Hello World")
+const express=require('express')
+const dotenv=require('dotenv')
+const port=process.env.port || 3000
+const app=express()
+
+app.listen(port,()=>{
+console.log(`App running on port ${port}`);
+});
